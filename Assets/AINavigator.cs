40c2a47ed18abed3dd422b0f3a87e22d498b951f
Jesper Tingvall <jesper@tingvall.pw>
2015-04-11 01:11:28 +0200

    Added AI

---
+++ Assets/AINavigator.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AINavigator : MonoBehaviour {
+
+	private NavMeshAgent agent;
+
+	
+	public TurretScript turret;
+
+
+	public Transform goal;
+	
+	void Start () {
+		agent = GetComponent<NavMeshAgent>();
+
+	}
+
+	void FixedUpdate() {
+		var target = turret.GetVisualTarget ();
+		if (target && target.GetComponent<HealthComponent>() != null) {
+			turret.Fire();
+		}
+		agent.destination = goal.position; 
+	}
+}
