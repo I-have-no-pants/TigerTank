31c2e1ca6adbd664f4ba7dcbefbd06eb589cf653
Jesper Tingvall <jesper@tingvall.pw>
2015-04-10 22:17:33 +0200

    Add Health, bullet and explosion script

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	public int Swedishness; // Life
+
+	public enum GameState {Running, GameOver};
+
+	public GameState State;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+}
---
+++ Assets/Scripts/HealthComponent.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthComponent : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	public float _MAXHEALTH;
+	public float health;
+	public float Health {
+		get {
+			return health;
+		}
+		set {
+			if (value <= 0) {
+				health = 0;
+				SendMessage("OnDeath");
+				Debug.Log(gameObject + " died!");
+				Destroy(this);
+
+			} else {
+				SendMessage("OnDamage",value,SendMessageOptions.DontRequireReceiver);
+				health = value;
+				Debug.Log(gameObject + " health: " + health);
+			}
+		}
+	}
+}
