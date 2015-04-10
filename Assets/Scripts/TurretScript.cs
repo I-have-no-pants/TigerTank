f56e917dbef145c9ebc66e005ba2939fbe6f34da
Jesper Tingvall <jesper@tingvall.pw>
2015-04-10 23:04:40 +0200

    Add dual monitor support

---
+++ Assets/Scripts/TurretScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurretScript : MonoBehaviour {
+
+	public BulletScript bullet;
+	public Transform muzzle;
+	public float Velocity;
+	
+	public void Fire() {
+		BulletScript b = Instantiate (bullet, muzzle.position, muzzle.rotation) as BulletScript;
+		b.turretCollider = GetComponent<Collider> ();
+		b.GetComponent<Rigidbody> ().velocity = Velocity * muzzle.forward;
+	}
+}
