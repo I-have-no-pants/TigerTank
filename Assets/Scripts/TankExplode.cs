64f46f4e7c12a5e229f0954d1ffb8f7133306c36
Jesper Tingvall <jesper@tingvall.pw>
2015-04-11 12:06:52 +0200

    Enemy tank turret dies

--- Assets/Scripts/TankExplode.cs
+++ Assets/Scripts/TankExplode.cs
@@ -9,5 +9,8 @@ public class TankExplode : MonoBehaviour {
 		Destroy (GetComponent<NavMeshAgent>());
 		foreach (var r in GetComponentsInParent<Rigidbody>())
 			r.isKinematic = false;
+
+		foreach (var t in GetComponentsInChildren<TurrentTarget>())
+			Destroy (t);
 	}
 }
