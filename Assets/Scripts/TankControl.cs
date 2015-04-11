fd0fd35243a41d1d1d5b2c9abe68cee0e83cd1df
Jesper Tingvall <jesper@tingvall.pw>
2015-04-11 17:16:41 +0200

    TigerTankMoves

--- Assets/Scripts/TankControl.cs
+++ Assets/Scripts/TankControl.cs
@@ -3,35 +3,22 @@ using System.Collections;
 using UnityStandardAssets.CrossPlatformInput;
 
 public class TankControl : MonoBehaviour {
-	WheelCollider[] m_WheelColliders;
-	WheelCollider[] m_WheelCollidersLeft;
-	WheelCollider[] m_WheelCollidersRight;
-	float m_CurrentTorque;
+	public WheelCollider[] m_WheelCollidersLeft;
+	public WheelCollider[] m_WheelCollidersRight;
+	public float Speed;
 
 	// Use this for initialization
 	void Start () {
-		m_WheelColliders = GetComponentsInChildren<WheelCollider> ();
 
-		m_WheelCollidersLeft = new WheelCollider[4];
-		m_WheelCollidersRight = new WheelCollider[4];
-		for(int i = 0; i < m_WheelColliders.Length; i++) {
-			if(m_WheelColliders[i].name.Contains ("left")) {
-				m_WheelCollidersLeft[i % 4] = m_WheelColliders[i];
-			} else {
-				m_WheelCollidersRight[i % 4] = m_WheelColliders[i];
-			}		
-		}
-
-		m_CurrentTorque = 300;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		float threadsLeft = CrossPlatformInputManager.GetAxis ("Horizontal");
-		float threadsRight = CrossPlatformInputManager.GetAxis ("Vertical");
+		float threadsLeft = CrossPlatformInputManager.GetAxis ("TrackLeft");
+		float threadsRight = CrossPlatformInputManager.GetAxis ("TrackRight");
 
-		float leftThrustTorque = threadsLeft * (m_CurrentTorque / (float)m_WheelCollidersLeft.Length);
-		float rightThrustTorque = threadsRight * (m_CurrentTorque / (float)m_WheelCollidersRight.Length);
+		float leftThrustTorque = threadsLeft * Speed;
+		float rightThrustTorque = threadsRight * Speed;
 
 		for (int i = 0; i < m_WheelCollidersLeft.Length; i++) {
 			m_WheelCollidersLeft[i].motorTorque = leftThrustTorque;
