44e4854df9c7c5ef17518b6c7edfae29399f76c3
woozie164 <woozie663@gmail.com>
2015-04-11 13:59:56 +0200

    Created new tank prefab
    Added the tank models and more wheels (more stable)

--- Assets/Scripts/TankControl.cs
+++ Assets/Scripts/TankControl.cs
@@ -4,10 +4,24 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class TankControl : MonoBehaviour {
 	WheelCollider[] m_WheelColliders;
+	WheelCollider[] m_WheelCollidersLeft;
+	WheelCollider[] m_WheelCollidersRight;
 	float m_CurrentTorque;
+
 	// Use this for initialization
 	void Start () {
 		m_WheelColliders = GetComponentsInChildren<WheelCollider> ();
+
+		m_WheelCollidersLeft = new WheelCollider[4];
+		m_WheelCollidersRight = new WheelCollider[4];
+		for(int i = 0; i < m_WheelColliders.Length; i++) {
+			if(m_WheelColliders[i].name.Contains ("left")) {
+				m_WheelCollidersLeft[i % 4] = m_WheelColliders[i];
+			} else {
+				m_WheelCollidersRight[i % 4] = m_WheelColliders[i];
+			}		
+		}
+
 		m_CurrentTorque = 300;
 	}
 	
@@ -16,11 +30,15 @@ public class TankControl : MonoBehaviour {
 		float threadsLeft = CrossPlatformInputManager.GetAxis ("Horizontal");
 		float threadsRight = CrossPlatformInputManager.GetAxis ("Vertical");
 
-		float leftThrustTorque = threadsLeft * (m_CurrentTorque / 2f);
-		float rightThrustTorque = threadsRight * (m_CurrentTorque / 2f);
-		// Assume wheel 0 and 1 are on the same side.
-		m_WheelColliders[0].motorTorque = m_WheelColliders[1].motorTorque = leftThrustTorque;
-		// Assume wheel 2 and 3 are on the same side.
-		m_WheelColliders[2].motorTorque = m_WheelColliders[3].motorTorque = rightThrustTorque;
+		float leftThrustTorque = threadsLeft * (m_CurrentTorque / (float)m_WheelCollidersLeft.Length);
+		float rightThrustTorque = threadsRight * (m_CurrentTorque / (float)m_WheelCollidersRight.Length);
+
+		for (int i = 0; i < m_WheelCollidersLeft.Length; i++) {
+			m_WheelCollidersLeft[i].motorTorque = leftThrustTorque;
+		}
+
+		for (int i = 0; i < m_WheelCollidersRight.Length; i++) {
+			m_WheelCollidersRight[i].motorTorque = rightThrustTorque;
+		}
 	}
 }
