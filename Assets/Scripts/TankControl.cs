9c6ded8bd02136bc4e20dbae950803cfca72b0f9
Jesper Tingvall <jesper@tingvall.pw>
2015-04-11 16:21:26 +0200

    Add player tank

--- Assets/Scripts/TankControl.cs
+++ Assets/Scripts/TankControl.cs
@@ -3,24 +3,29 @@ using System.Collections;
 using UnityStandardAssets.CrossPlatformInput;
 
 public class TankControl : MonoBehaviour {
-	WheelCollider[] m_WheelColliders;
-	float m_CurrentTorque;
+	public WheelCollider[] LeftWheels;
+	public WheelCollider[] RightWheels;
+
+	public float Speed;
+
+	float rightTorque;
+	float leftTorque;
 	// Use this for initialization
 	void Start () {
-		m_WheelColliders = GetComponentsInChildren<WheelCollider> ();
-		m_CurrentTorque = 300;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		float threadsLeft = CrossPlatformInputManager.GetAxis ("Horizontal");
-		float threadsRight = CrossPlatformInputManager.GetAxis ("Vertical");
+	void FixedUpdate () {
+		float threadsLeft = CrossPlatformInputManager.GetAxis ("TrackLeft");
+		float threadsRight = CrossPlatformInputManager.GetAxis ("TrackRight");
 
-		float leftThrustTorque = threadsLeft * (m_CurrentTorque / 2f);
-		float rightThrustTorque = threadsRight * (m_CurrentTorque / 2f);
-		// Assume wheel 0 and 1 are on the same side.
-		m_WheelColliders[0].motorTorque = m_WheelColliders[1].motorTorque = leftThrustTorque;
-		// Assume wheel 2 and 3 are on the same side.
-		m_WheelColliders[2].motorTorque = m_WheelColliders[3].motorTorque = rightThrustTorque;
+		float leftThrustTorque = threadsLeft * Speed;
+		float rightThrustTorque = threadsRight * Speed;
+
+		foreach (var w in LeftWheels)
+			w.motorTorque = leftThrustTorque;
+
+		foreach (var w in RightWheels)
+			w.motorTorque = rightThrustTorque;
 	}
 }
