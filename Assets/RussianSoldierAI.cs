5d23e28d964e5e13e0b51ff72e35865f40c84a04
Jesper Tingvall <jesper@tingvall.pw>
2015-04-11 14:47:32 +0200

    Russian guys die in blood

---
+++ Assets/RussianSoldierAI.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RussianSoldierAI : MonoBehaviour {
+
+	
+	public TurretScript turret;
+	private AINavigator navigator;
+	
+	public HealthComponent target;
+	
+	public float distance;
+	
+	public int EnemyTeam;
+	
+	public float ReloadTime;
+	private float reloadTimer;
+	
+	void Start() {
+		navigator = GetComponent<AINavigator> ();
+	}
+	
+	// Logic for sorting
+	public HealthComponent FindTarget() {
+		foreach(var h in FindObjectsOfType<HealthComponent>()) {
+			if (h.Team == EnemyTeam)
+				return h;
+		}
+		return null;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate() {
+		if (reloadTimer > 0)
+			reloadTimer -= Time.fixedDeltaTime;
+		
+		if (target == null || target.IsDead)
+			target = FindTarget ();
+		else {
+			if (!AINavigator.CanReachTargetFrom (navigator.navTarget.position, target.gameObject)) {
+				Vector2 sphere = Random.insideUnitCircle;
+				Vector3 sphere2 = new Vector3 (sphere.x, 0, sphere.y);
+				navigator.navTarget.position = target.transform.position + sphere2 * distance;
+			}
+		}
+		
+		// Calculate turret's target
+		var turretTarget = turret.GetVisualTarget ();
+		if (turretTarget) {
+			var h = turretTarget.GetComponent<HealthComponent> ();
+			if (h == null)
+				h = turretTarget.GetComponentInParent<HealthComponent> ();
+			
+			if (h && h.Team == EnemyTeam) {
+				if (reloadTimer <= 0) {
+					turret.Fire ();
+					reloadTimer = ReloadTime;
+				}
+			}
+		}
+	}
+}
---
+++ Assets/RussianSoldierDeath.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RussianSoldierDeath : MonoBehaviour {
+
+	public GameObject Blood;
+
+	public void OnDeath() {
+		FindObjectOfType<SpawnerScript> ().EnemyKilled ();
+		
+		Destroy (GetComponent<RussianSoldierAI>());
+		Destroy (GetComponent<AINavigator>());
+		Destroy (GetComponent<NavMeshAgent>());
+		foreach (var r in GetComponentsInParent<Rigidbody>())
+			r.isKinematic = false;
+		
+		foreach (var t in GetComponentsInChildren<TurrentTarget>())
+			Destroy (t);
+		Instantiate (Blood, transform.position, transform.rotation);
+
+		Destroy (gameObject);
+	}
+}
