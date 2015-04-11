123ea5b0be52f007b7e7949ed15641d57b7aa65e
Jesper Tingvall <jesper@tingvall.pw>
2015-04-11 12:05:25 +0200

    Move files

---
+++ Assets/Scripts/TankExplode.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TankExplode : MonoBehaviour {
+
+	public void OnDeath() {
+		Destroy (GetComponent<AITactics>());
+		Destroy (GetComponent<AINavigator>());
+		Destroy (GetComponent<NavMeshAgent>());
+		foreach (var r in GetComponentsInParent<Rigidbody>())
+			r.isKinematic = false;
+	}
+}
--- Assets/TankExplode.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class TankExplode : MonoBehaviour {
-
-	public void OnDeath() {
-		Destroy (gameObject);
-	}
-}
